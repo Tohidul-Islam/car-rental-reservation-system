169a57639be331f887cacdb1c308b5f831b7d6db
Tohidul-Islam <tohidulislam34469@gmail.com>
2024-06-13 08:08:30 +0600

    second

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import router from './app/modules/Routes';
+const app: Application = express();
+
+//parser
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173'] }));
+
+// application router
+app.use('/api/v1', router);
+
+const test = async (req: Request, res: Response) => {
+  // Promise.reject();
+  res.send('Hello World!');
+};
+app.get('/', test);
+
+export default app;
---
+++ src/app/modules/Routes/index.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: "/cars",
+    // route: ,
+  },
+  {
+    path: "/bookings",
+    // route: ,
+  },
+  {
+    path: "/admins",
+    // route: ,
+  },
+  {
+    path: "/auth",
+    // route: ,
+  },
+];
+
+// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
